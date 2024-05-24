ab13fe5befc5795e2fe2867bba6d4552567108a9
Jesse Cusack <jesse.cusack@oregonstate.edu>
2024-05-23 19:11:42 -0700

    no need for install/remove env files

--- remove_environment.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source $CONDA_PREFIX/etc/profile.d/conda.sh
-conda activate lcpp && jupyter kernelspec uninstall lcpp && conda deactivate
-conda remove --name lcpp --all
-conda activate lcpp-dev && jupyter kernelspec uninstall lcpp-dev && conda deactivate
-conda remove --name lcpp-dev --all
